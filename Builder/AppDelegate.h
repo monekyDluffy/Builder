ab7d6c97b92bff17d9be0aad253b7b2909f3d4ac
yusheng <yusheng.liu@lianbi.com.cn>
2018-03-09 16:29:07 +0800

    Initial Commit

---
+++ Builder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Builder
+//
+//  Created by yusheng on 2018/3/9.
+//  Copyright © 2018年 yusheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
